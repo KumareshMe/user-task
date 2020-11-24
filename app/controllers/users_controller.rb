e681482620ecf83f88e7c4b85d1e0643ab28161c
Kumaresh <kumareshme2@gmail.com>
2020-11-24 19:22:04 +0530

    Add users show page

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,10 @@
 class UsersController < ApplicationController
     
+    def show
+        @user = User.find(params[:id])
+        @tasks = @user.tasks
+    end
+
     def new
         @user = User.new
     end
