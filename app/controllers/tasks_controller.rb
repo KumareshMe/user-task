68f9c44a985f4ad1351f03c6cb0b206fd5baf2ba
Kumaresh <kumareshme2@gmail.com>
2020-11-24 14:55:34 +0530

    Add one-to-many association between users and tasks from back end

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -14,6 +14,7 @@ class TasksController < ApplicationController
 
     def create
       @task = Task.new(task_params)
+      @task.user = User.first
       if @task.save
         flash[:notice] ="Task was created successfully."
         redirect_to @task
--- app/models/task.rb
+++ app/models/task.rb
@@ -1,5 +1,6 @@
 class Task < ApplicationRecord 
 
+  belongs_to :user
   validates :title, presence: true, length: { minimum:3, maximum:100 }
   validates :description, presence: true, length: { minimum:6, maximum:300 }
 
